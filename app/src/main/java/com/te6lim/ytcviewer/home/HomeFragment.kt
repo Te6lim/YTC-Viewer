f5e8d756abb9a5df063ae84b2c24ac9cd3358ae9
te6lim <teslimakande@gmail.com>
2022-01-27 00:04:38 +0100

    dark mode redesign

--- app/src/main/java/com/te6lim/ytcviewer/home/HomeFragment.kt
+++ app/src/main/java/com/te6lim/ytcviewer/home/HomeFragment.kt
@@ -1,9 +1,8 @@
 package com.te6lim.ytcviewer.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -14,9 +13,14 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
+    //Test (DarkMode switch)
+    private var isDarkMode = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
+        setHasOptionsMenu(true)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_home, container, false
         )
@@ -26,5 +30,23 @@ class HomeFragment : Fragment() {
 
         }
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        return inflater.inflate(R.menu.home_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.theme_selection -> {
+                //Test (Light and Dark mode activation)
+                if (isDarkMode) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                isDarkMode = !isDarkMode
+                true
+            }
+            else -> false
+        }
     }
 }
