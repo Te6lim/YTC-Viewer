2fd66698ab0b1d4d3bfb6c6d9ad98c1126fabda3
te6lim <teslimakande@gmail.com>
2022-01-26 15:16:42 +0100

    fix search bar positioning in the home fragment

--- app/src/main/java/com/te6lim/ytcviewer/home/cards/CardsFragment.kt
+++ app/src/main/java/com/te6lim/ytcviewer/home/cards/CardsFragment.kt
@@ -1,6 +1,17 @@
 package com.te6lim.ytcviewer.home.cards
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.te6lim.ytcviewer.R
 
 class CardsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, sacedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cards, container, false)
+    }
 }
