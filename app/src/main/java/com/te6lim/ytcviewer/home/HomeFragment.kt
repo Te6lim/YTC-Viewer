0a862561fa4532187996ee9151ea18cf513a2435
te6lim <teslimakande@gmail.com>
2022-01-26 15:55:41 +0100

    fix search bar positioning in the home fragment

--- app/src/main/java/com/te6lim/ytcviewer/home/HomeFragment.kt
+++ app/src/main/java/com/te6lim/ytcviewer/home/HomeFragment.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.te6lim.ytcviewer.R
+import com.te6lim.ytcviewer.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home, container, false
+        )
+
+        binding.searchBar.setOnClickListener {
+            (it as SearchView)
+
+        }
+        return binding.root
     }
 }
