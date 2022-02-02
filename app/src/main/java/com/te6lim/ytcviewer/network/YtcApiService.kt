71307f69718ca3c8b4e2f69d0ea6d8df200c390a
te6lim <teslimakande@gmail.com>
2022-02-02 01:06:53 +0100

    Setting up Ytc api service

---
+++ app/src/main/java/com/te6lim/ytcviewer/network/YtcApiService.kt
@@ -0,0 +1,21 @@
+package com.te6lim.ytcviewer.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://db.ygoprodeck.com/api/v7/"
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface YtcApiService {
+
+    @GET("cardinfo.php")
+    fun getCards(): String
+}
+
+object YtcApi {
+    val retrofitService: YtcApiService by lazy { retrofit.create(YtcApiService::class.java) }
+}
