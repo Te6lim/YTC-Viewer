b2afd982d3e693e41cb49e104569cb8176d4a0c0
te6lim <teslimakande@gmail.com>
2022-02-05 19:04:25 +0100

    color fixes

--- app/src/main/java/com/te6lim/ytcviewer/home/cards/CardsViewModel.kt
+++ app/src/main/java/com/te6lim/ytcviewer/home/cards/CardsViewModel.kt
@@ -3,9 +3,7 @@ package com.te6lim.ytcviewer.home.cards
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.te6lim.ytcviewer.network.YtcApi
-import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,24 +19,21 @@ class CardsViewModel : ViewModel() {
     }
 
     private fun getProperties() {
-        viewModelScope.launch {
-            YtcApi.retrofitService.getCardsAsync().enqueue(object : Callback<String> {
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    _propertiesString.value = response.body()
-                }
+        YtcApi.retrofitService.getCardsAsync().enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _propertiesString.value = "success"
+            }
 
-                override fun onFailure(call: Call<String>, t: Throwable) {
-                    _propertiesString.value = t.message
-                }
-
-            })
-
-            /*try {
-                _propertiesString.value = string.await()
-
-            } catch (t: Throwable) {
+            override fun onFailure(call: Call<String>, t: Throwable) {
                 _propertiesString.value = t.message
-            }*/
-        }
+            }
+        })
+
+        /*try {
+            _propertiesString.value = string.await()
+
+        } catch (t: Throwable) {
+            _propertiesString.value = t.message
+        }*/
     }
 }
--- app/src/main/java/com/te6lim/ytcviewer/network/YtcApiService.kt
+++ app/src/main/java/com/te6lim/ytcviewer/network/YtcApiService.kt
@@ -4,6 +4,7 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val BASE_URL = "https://db.ygoprodeck.com/api/v7/"
 private val retrofit = Retrofit.Builder()
@@ -14,7 +15,9 @@ private val retrofit = Retrofit.Builder()
 interface YtcApiService {
 
     @GET("cardinfo.php")
-    fun getCardsAsync(): Call<String>
+    fun getCardsAsync(
+        @Query("type") type: String = "normal monster"
+    ): Call<String>
 }
 
 object YtcApi {
