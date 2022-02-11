9c2024723caa2ecb8042e929e263dd1412c95f10
te6lim <teslimakande@gmail.com>
2022-02-11 01:27:28 +0100

    setup filter selection lists in HomeViewModel.kt

--- app/src/main/java/com/te6lim/ytcviewer/home/HomeViewModel.kt
+++ app/src/main/java/com/te6lim/ytcviewer/home/HomeViewModel.kt
@@ -6,6 +6,81 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
+    enum class CardFilter {
+        Type, Race, Attribute, Spell, Trap;
+    }
+
+    companion object {
+        fun getFilerSelections(filter: CardFilter): List<String> {
+            return when (filter) {
+                CardFilter.Type -> listOf(
+                    "Effect Monster",
+                    "Flip Effect Monster",
+                    "Flip Tuner Effect Monster",
+                    "Gemini Monster",
+                    "Normal Monster",
+                    "Normal Tuner Monster",
+                    "Pendulum Effect Monster",
+                    "Pendulum Flip Effect Monster",
+                    "Pendulum Normal Monster",
+                    "Pendulum Tuner Effect Monster",
+                    "Ritual Effect Monster",
+                    "Ritual Monster",
+                    "Skill Card",
+                    "Spirit Monster",
+                    "Toon Monster",
+                    "Tuner Monster",
+                    "Union Effect Monster"
+                )
+
+                CardFilter.Race -> listOf(
+                    "Aqua",
+                    "Beast",
+                    "Beast-Warrior",
+                    "Creator-God",
+                    "Cyberse",
+                    "Dinosaur",
+                    "Divine-Beast",
+                    "Dragon",
+                    "Fairy",
+                    "Fiend",
+                    "Fish",
+                    "Insect",
+                    "Machine",
+                    "Plant",
+                    "Psychic",
+                    "Pyro",
+                    "Reptile",
+                    "Rock",
+                    "Sea Serpent",
+                    "Spellcaster",
+                    "Thunder",
+                    "Warrior",
+                    "Winged Beast"
+                )
+
+                CardFilter.Attribute -> listOf(
+
+                )
+
+                CardFilter.Spell -> listOf(
+                    "Normal",
+                    "Field",
+                    "Equip",
+                    "Continuous",
+                    "Quick-Play",
+                    "Ritual"
+                )
+
+                CardFilter.Trap -> listOf(
+                    "Normal",
+                    "Continuous",
+                    "Counter"
+                )
+            }
+        }
+    }
+
     //Test Navigation
     private val _navigateToDetailScreen = MutableLiveData<Boolean>()
     val navigateToDetailScreen: LiveData<Boolean>
