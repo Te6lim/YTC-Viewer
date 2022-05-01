f671a804d0186c37af9e4f5e5c8dbd7603b5399d
te6lim <teslimakande@gmail.com>
2022-05-01 12:04:00 +0100

    FilterSelectionActivity.kt initialization

---
+++ app/src/main/java/com/te6lim/ytcviewer/filters/FilterSelectionActivity.kt
@@ -0,0 +1,21 @@
+package com.te6lim.ytcviewer.filters
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.te6lim.ytcviewer.R
+import com.te6lim.ytcviewer.databinding.ActivitySelectionFilterBinding
+
+class FilterSelectionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySelectionFilterBinding
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_selection_filter)
+
+        setSupportActionBar(binding.toolbar)
+
+    }
+}
