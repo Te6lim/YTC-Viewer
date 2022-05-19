ac93d7615828da5d63ce6483ee50c881484f0c0f
te6lim <teslimakande@gmail.com>
2022-05-19 18:51:32 +0100

    designed retry button

--- app/src/main/java/com/te6lim/ytcviewer/details/CardDetailsViewModel.kt
+++ app/src/main/java/com/te6lim/ytcviewer/details/CardDetailsViewModel.kt
@@ -1,14 +1,11 @@
 package com.te6lim.ytcviewer.details
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.te6lim.ytcviewer.domain.Card
 
 class CardDetailsViewModel(val card: Card) : ViewModel() {
-    private val liveDataCard: LiveData<Card>
-        get() = MutableLiveData(card)
+
 }
 
 class CardDetailsViewModelFactory(private val card: Card) : ViewModelProvider.Factory {
