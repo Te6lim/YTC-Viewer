48776ebca349731850b91fcea8be2d8024250b20
te6lim <teslimakande@gmail.com>
2022-05-30 19:53:56 +0100

    cached theme mode setting

--- app/src/main/java/com/te6lim/ytcviewer/YTCApplication.kt
+++ app/src/main/java/com/te6lim/ytcviewer/YTCApplication.kt
@@ -1,14 +1,33 @@
 package com.te6lim.ytcviewer
 
 import android.app.Application
-import kotlin.properties.Delegates
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 
 class YTCApplication : Application() {
 
-    var toDarkMode by Delegates.notNull<Boolean>()
+    companion object {
+        private const val THEME_MODE_KEY = "darkMode"
+    }
+
+    private lateinit var preference: SharedPreferences
+
+    var toDarkMode = false
+        set(value) {
+            field = value
+            preference.edit().apply {
+                putBoolean(THEME_MODE_KEY, value)
+            }.apply()
+        }
 
     override fun onCreate() {
         super.onCreate()
-        toDarkMode = false
+        preference = getSharedPreferences(getString(R.string.preferenceFile), Context.MODE_PRIVATE)
+        toDarkMode = preference.getBoolean(THEME_MODE_KEY, false)
+        if (toDarkMode) AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        else AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
     }
 }
